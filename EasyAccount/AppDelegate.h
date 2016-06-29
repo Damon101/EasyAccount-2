821ce8cefda07c729c5e77a151fa928cb731a4fd
张威 <Kin@zhangweidemacbook-pro.local>
2016-06-29 09:49:38 +0800

    Initial Commit

---
+++ EasyAccount/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EasyAccount
+//
+//  Created by Kirito on 16/6/29.
+//  Copyright © 2016年 Kirito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
